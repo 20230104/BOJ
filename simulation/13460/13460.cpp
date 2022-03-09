da608c8e61bd99f192c24314a77eae124119654c
javaboooom <58615028+javaboooom@users.noreply.github.com>
2022-03-09 05:32:13 +0000

    13460solved

--- simulation/13460/13460.cpp
+++ simulation/13460/13460.cpp
@@ -7,10 +7,12 @@ using namespace std;
 
 class s
 {
-    typedef struct{
+    typedef struct
+    {
         pair<int, int> r, b;
-        int l;
-    }factor;
+        int d, l;
+    } factor;
+
 private:
     int N, M;
     pair<int, int> R, B;
@@ -19,54 +21,40 @@ private:
 
     bool incline(pair<int, int> &r, pair<int, int> &b, int d)
     {
-        pair<int, int> nr;
-        pair<int, int> nb;
-        switch (d)
+        pair<int, int> nr = MOVE[r.first][r.second][d];
+        pair<int, int> nb = MOVE[b.first][b.second][d];
+        if (nr == nb && BRD[nr.first][nr.second] != 'O')
         {
-        case 0:
-            nr = MOVE[r.first][r.second][0];
-            nb = MOVE[b.first][b.second][0];
-            if (nr == nb && BRD[nr.first][nr.second] != 'O')
+            switch (d)
             {
+            case 0:
                 if (r.first < b.first)
                     nb.first++;
                 else
                     nr.first++;
-            }
-            break;
-        case 1:
-            nr = MOVE[r.first][r.second][1];
-            nb = MOVE[b.first][b.second][1];
-            if (nr == nb && BRD[nr.first][nr.second] != 'O')
-            {
+                break;
+            case 1:
                 if (r.second < b.second)
                     nr.second--;
                 else
                     nb.second--;
-            }
-        case 2:
-            nr = MOVE[r.first][r.second][2];
-            nb = MOVE[b.first][b.second][2];
-            if (nr == nb && BRD[nr.first][nr.second] != 'O')
-            {
+                break;
+            case 2:
                 if (r.first < b.first)
                     nr.first--;
                 else
                     nb.first--;
-            }
-        case 3:
-            nr = MOVE[r.first][r.second][3];
-            nb = MOVE[b.first][b.second][3];
-            if (nr == nb && BRD[nr.first][nr.second] != 'O')
-            {
+                break;
+            case 3:
                 if (r.second < b.second)
                     nb.second++;
                 else
                     nr.second++;
-            }
+                break;
 
-        default:
-            break;
+            default:
+                break;
+            }
         }
         r = nr, b = nb;
         if (BRD[nr.first][nr.second] == 'O' || BRD[nb.first][nb.second] == 'O')
@@ -79,9 +67,9 @@ private:
     {
         MOVE.assign(N, vector<vector<pair<int, int>>>(M, vector<pair<int, int>>(4)));
 
-        for (int i = 0; i < N; i++)
+        for (int i = 1; i < N - 1; i++)
         {
-            for (int j = 0; j < M; j++)
+            for (int j = 1; j < M - 1; j++)
             {
                 int y = i, x = j;
                 while (BRD[y][j] != '#')
@@ -161,41 +149,49 @@ public:
         }
     }
 
-    void ution(){
+    void ution()
+    {
         makeMoveTable();
         queue<factor> q;
         factor f;
-        f.l = 0, f.r = R, f.b = B;
+        f.l = 0, f.r = R, f.b = B, f.d = -1;
         q.push(f);
 
-        while(!q.empty()){
+        while (!q.empty())
+        {
             f = q.front();
             q.pop();
-            if(f.l == 10){
+            if (f.l == 10)
+            {
                 printf("-1\n");
                 return;
             }
-            pair<int, int> nb, nr;  
-            for(int d = 0;d<4;d++){
+
+            pair<int, int> nb, nr;
+            for (int d = 0; d < 4; d++)
+            {
+                if(d == f.d) continue;
                 nb = f.b, nr = f.r;
-                if(incline(nr, nb, d)){
-                    if(BRD[nb.first][nb.second] != 'O'){
+                if (incline(nr, nb, d))
+                {
+                    if (BRD[nb.first][nb.second] != 'O')
+                    {
                         printf("%d\n", f.l + 1);
                         return;
                     }
                 }
-                else{
+                else
+                {
                     factor nf;
                     nf.l = f.l + 1;
                     nf.r = nr;
                     nf.b = nb;
+                    nf.d = d;
                     q.push(nf);
                 }
             }
         }
-
     }
-
 
     void show()
     {
@@ -212,14 +208,29 @@ public:
         printf("R : %d, %d\nB : %d, %d\n", R.first, R.second, B.first, B.second);
         printf("\n");
         printf("\n");
+
+        printf("=====MOVE=====\n");
+        for (int i = 1; i < N - 1; i++)
+        {
+            for (int j = 1; j < M - 1; j++)
+            {
+                printf("%d, %d\n", i, j);
+                for (int d = 0; d < 4; d++)
+                {
+                    printf("(%d, %d) ", MOVE[i][j][d].first, MOVE[i][j][d].second);
+                }
+                printf("\n");
+            }
+        }
     }
 };
 
 int main()
 {
-    freopen("01.txt", "r", stdin);
+    freopen("06.txt", "r", stdin);
     s sol;
     sol.ution();
+    // sol.show();
 
     return 0;
 }
