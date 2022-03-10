b799f1f5165bb0f6e72950da6977dc0f5d1f0db5
javaboooom <58615028+javaboooom@users.noreply.github.com>
2022-03-10 23:53:57 +0900

    Update 17281.cpp

--- simulation/17281/17281.cpp
+++ simulation/17281/17281.cpp
@@ -4,81 +4,70 @@
 
 using namespace std;
 
-class s{
-    private:
+class s {
+  private:
     int N, M = 0;
     vector<vector<int>> BRD;
-    int calScore(int p[9]){
-        int start = 0, ret = 0;
-        for(int i = 0 ; i<N;i++){
-            if(M>=ret + (N - i)*24) return 0;
-            int tmp[3];
-            int cnt = 0, out = 0, t = start;
-            while(out!=3){
-                if(BRD[i][p[t%9]] == 0){
-                    out++;
-                }
-                else{
-                    tmp[cnt%3] = BRD[i][p[t%9]];
-                    cnt++;
-                }
-                t++;
-            }
-            int las = 0;
-            for(int c = 1; c<4;c++){
-                if(cnt - c <0){
-                    cnt = 0;
-                    break;
-                }
-                las+=tmp[(cnt - c) % 3];
-                if(las >= 4){
-                    cnt -= c - 1;
-                    break;
-                }
-            }
-            if(cnt >= 3&&las == 3)cnt -=3 ;
-            ret+=cnt;
-            start = t%9;
+
+  int calScore(int p[9]) {
+    int start = 0, ret = 0;
+    for (int i = 0; i < N; i++) {
+      if (M >= ret + (N - i) * 24) return 0;
+      int out = 0, t = start;
+      while (out != 3) {
+        if (BRD[i][p[t % 9]] == 0) {
+          out++;
         }
-        return ret;
-    }
-    public:
-    s(){
-        scanf("%d", &N);
-        BRD.assign(N, vector<int>(9));
-        for(int i = 0 ; i<N;i++){
-            for(int j = 0 ; j<9;j++){
-                scanf("%d", &BRD[i][j]);
-            }
+        t++;
+      }
+      int cnt = t - start - 3, las = 0;
+      int tmp_start = t % 9;
+      t--;
+      for (int c = 0; c < 3; c++, t--) {
+        while (t >= start && BRD[i][p[t % 9]] == 0) {
+          t--;
         }
+        if (t < start) {
+          cnt = 0;
+          break;
+        }
+        las += BRD[i][p[t % 9]];
+        if (las >= 4) {
+          cnt -= c;
+          break;
+        }
+      }
+      if (cnt >= 3 && las == 3) cnt -= 3;
+      ret += cnt;
+      start = tmp_start;
     }
-    void ution(){
-        int semi_p[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-        do{
-            int p[9];
-            for(int i = 0 ; i<3;i++) p[i] = semi_p[i];
-            p[3] = 0;
-            for(int i = 4;i<9;i++) p[i] = semi_p[i-1];
-            //show(p);
-            M = max(M, calScore(p));
-        }while(next_permutation(semi_p, semi_p + 8));
-        printf("%d", M);
+    return ret;
+  }
+  public:
+    s() {
+      scanf("%d", & N);
+      BRD.assign(N, vector < int > (9));
+      for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 9; j++) {
+          scanf("%d", & BRD[i][j]);
+        }
+      }
     }
-    
-    void test(){
-        int p[9] = {0, 1, 2,3,4,5,6,7,8};
-        printf("%d", calScore(p));
-    }
-    
-    void show(int p[]){
-        for(int i = 0 ; i<9;i++) printf("%d ", p[i]);
-        printf("\n");
-    }
+  void ution() {
+    int semi_p[8] = {1,2,3,4,5,6,7,8};
+    do {
+      int p[9];
+      for (int i = 0; i < 3; i++) p[i] = semi_p[i];
+      p[3] = 0;
+      for (int i = 4; i < 9; i++) p[i] = semi_p[i - 1];
+      M = max(M, calScore(p));
+    } while (next_permutation(semi_p, semi_p + 8));
+    printf("%d", M);
+  }
 };
 
-int main()
-{
-    s sol;
-    sol.ution();
-    return 0;
+int main() {
+  s sol;
+  sol.ution();
+  return 0;
 }
