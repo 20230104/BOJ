5b07bd4fffbd852db2412555959e94d33806471a
javaboooom <58615028+javaboooom@users.noreply.github.com>
2022-03-13 05:06:31 +0000

    16235solved

--- simulation/16235/16235.cpp
+++ simulation/16235/16235.cpp
@@ -1,169 +1,169 @@
 #include <cstdio>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-class s
-{
+class s{
 private:
-    int N, M, K, T;
-    vector<vector<int>> DLT, FOD;
-    vector<vector<priority_queue<pair<int, int>>>> GRD;
-
-    const int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-    const int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-    bool isValid(int x, int y)
-    {
-        if (0 <= x && x < N && 0 <= y && y < N)
-            return true;
-        return false;
-    }
-
+    const int dx[8] = {0, 0, 1, 1, 1, -1, -1, -1};
+    const int dy[8] = {1, -1, -1, 0, 1, -1, 0, 1};
+    int N, M, K;
+    vector<vector<vector<int>>> GROUND;
+    vector<vector<int>> FOOD, DELTA;
+    
+    int max(int x, int y){return x>y?x:y;}
+    bool isValid(int x, int y){return 0<=x&&x<N&&0<=y&&y<N;}
 public:
-    s()
-    {
+    s(){
         scanf("%d %d %d", &N, &M, &K);
-        T = 0;
-        DLT.assign(N, vector<int>(N));
-        FOD.assign(N, vector<int>(N, 5));
-        GRD.assign(N, vector<priority_queue<pair<int, int>>>(N));
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                scanf("%d", &DLT[i][j]);
+        GROUND.assign(N, vector<vector<int>>(N, vector<int>(1111, 0)));
+        FOOD.assign(N, vector<int>(N, 5));
+        DELTA.assign(N, vector<int>(N));
+        
+        for(int i = 0 ; i<N;i++){
+            for(int j = 0 ; j<N;j++){
+                scanf("%d", &DELTA[i][j]);
             }
         }
-        for (int i = 0; i < M; i++)
-        {
+        
+        for(int i = 0 ; i<M;i++){
             int x, y, z;
             scanf("%d %d %d", &x, &y, &z);
-            GRD[x - 1][y - 1].push({1, -z});
+            GROUND[x-1][y-1][z]++;
+            GROUND[x-1][y-1][0] = max(GROUND[x-1][y-1][0], z);
         }
     }
-
-    void routine()
-    {
-        vector<pair<int, int>> baby;
-        for (int i = 0; i < N; i++) // spring and summer
-        {
-            for (int j = 0; j < N; j++)
-            {
-                auto &pq = GRD[i][j];
-                auto &food = FOD[i][j];
-                int n = pq.size();
-                while (n--)
-                {
-                    int f = -pq.top().second;
-                    pq.pop();
-                    if (f <= food)
-                    {
-                        if (f % 5 == 4)
-                        {
-                            baby.push_back({i, j});
-                        }
-                        food -= f;
-                        pq.push({-T, -f-1});
-                    }
-                    else
-                    {
-                        food += f / 2;
-                        while (n--)
-                        {
-                            auto f = -pq.top().second;
-                            pq.pop();
-                            food += f / 2;
-                        }
-                        n = 0;
-                    }
-                }
+    
+    void springAndSummer(int x, int y){
+        int &food = FOOD[x][y];
+        vector<int> &spot = GROUND[x][y];
+        int i = 1, m = 0, b = 0, cur = 0, flag = 0, size = spot[0];
+        for(i = 1 ; i<=size;i++){
+            cur = spot[i];
+            spot[i] = b;
+            if(b) m = i;
+            if(cur==0){//해당 나이의 나무가 존재하지 않음
+                b = 0;
+                continue;
             }
-        } // summer over
-
-        for (auto b : baby) // autumn
-        {
-            for (int d = 0; d < 8; d++)
-            {
-                int nx = b.first + dx[d], ny = b.second + dy[d];
-                if (isValid(nx, ny))
-                {
-                    GRD[nx][ny].push({-T, -1});
-                }
+            if(food<i*cur){//말라죽은 나무 존재 
+                flag = 1;
+                break;
             }
-        } // autumn over
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                FOD[i][j] += DLT[i][j];
+            else{//무럭무럭
+                food-=i*cur;
+            }
+            b = cur;
+        }
+        if(flag){//말라죽은 나무가 존재함
+            if(food>=i){//적어도 하나는 안죽음
+                spot[0] = i + 1;
+                b = food/i;
+                cur -= b;
+                food%=i;
+                food+=i/2*cur;
+            }
+            else{//모두 죽음
+                spot[0] = m;
+                food+=i/2*cur;
+                //spot[i] = 0;
+                b = spot[m];
+            }
+            for(i++;i<=size;i++){//이후로는 모두 퇴비화
+                food+=i/2*spot[i];
+                spot[i] = 0;
+            }
+            spot[spot[0]] = b;
+            
+        }
+        else{//모두 행복하게 먹음
+            spot[i] = b;
+            spot[0] = i;
+        }
+        if(spot[spot[0]]==0)spot[0] = 0;
+    }
+    void autumn(int x, int y){
+        int cnt = 0;
+        for(int i = 5;i<=GROUND[x][y][0];i+=5){
+            cnt+=GROUND[x][y][i];
+        }
+        for(int d = 0;d<8;d++){
+            int nx = x + dx[d], ny = y + dy[d];
+            if(isValid(nx, ny)&&cnt){
+                GROUND[nx][ny][1]+=cnt;
+                if(GROUND[nx][ny][0]==0)GROUND[nx][ny][0] = 1;
             }
         }
     }
-
-    void ution()
-    {
-        for (; T < K; T++)
-        {
-            routine();
-            //printf("DAY%d\n", T + 1);
-            //show();
-        }
-        int ret = 0;
-        for (auto G : GRD)
-        {
-            for (auto pq : G)
-            {
-                ret += pq.size();
-            }
-        }
-        printf("%d\n", ret);
+    void winter(int x, int y){
+        FOOD[x][y]+=DELTA[x][y];
+    }
+    void spotInfo(int x, int y){
+        printf("%d, %d's info\n", x, y);
+        printf("food:%d\n", FOOD[x][y]);
+        printf("size:%d\n", GROUND[x][y][0]);
+        for(int i = 1;i<=GROUND[x][y][0];i++)printf("%d:%d\n", i, GROUND[x][y][i]);
     }
 
-    void show()
-    {
-        // printf("delta\n");
-        // for (auto d : DLT)
-        // {
-        //     for (auto i : d)
-        //     {
-        //         printf("%d ", i);
+    void show(){
+        // printf("=====[0]=====\n");
+        // for(int i = 0 ; i<N;i++){
+        //     for(int j = 0 ; j<N;j++){
+        //         printf("%d ", GROUND[i][j][0]);
         //     }
         //     printf("\n");
         // }
-        // printf("\n");
-
-        printf("-----food-----\n");
-        for (auto F : FOD)
-        {
-            for (auto i : F)
-            {
-                printf("%d ", i);
+        printf("=====cnt=====\n");
+        for(int i = 0 ; i<N;i++){
+            for(int j = 0 ; j<N;j++){
+                int cnt = 0;
+                for(int k = 1;k<=GROUND[i][j][0];k++)cnt+=GROUND[i][j][k];
+                printf("%5d ", cnt);
             }
             printf("\n");
         }
-        printf("\n");
-
-        printf("-----youngest tree-----\n");
-        for (auto G : GRD)
-        {
-            for (auto pq : G)
-            {
-                printf("%3d ", pq.empty() ? 0 : -pq.top().second);
+    }
+    
+    void routine(){
+        for(int i = 0 ; i<N;i++){
+            for(int j = 0 ; j<N;j++){
+                springAndSummer(i, j);
             }
-            printf("\n");
         }
-        printf("\n");
+        for(int i = 0 ; i<N;i++){
+            for(int j = 0 ; j<N;j++){
+                autumn(i, j);
+            }
+        }
+        for(int i = 0 ; i<N;i++){
+            for(int j = 0 ; j<N;j++){
+                winter(i, j);
+            }
+        }
+    }
+    
+    void ution(){
+        for(int i = 0;i<K;i++){
+            //printf("DAY-%d\n", i + 1);            
+            //spotInfo(0, 1);
+            routine();
+            //spotInfo(7, 0);
+            //show();
+        }
+        int cnt = 0;
+        for(int i = 0 ; i<N;i++){
+            for(int j = 0 ; j<N;j++){
+                for(int s = 1;s<=GROUND[i][j][0];s++)cnt+=GROUND[i][j][s];
+            }
+        }
+        printf("%d\n", cnt);
     }
 };
 
 int main()
 {
     freopen("1000.txt", "r", stdin);
-
     s sol;
     sol.ution();
-
     return 0;
 }
